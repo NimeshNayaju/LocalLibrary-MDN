ee5ae79f886b81f05a71fad7c84823c2728e3384
NimeshNayaju <nimeshnayaju@gmail.com>
2018-04-27 18:45:23 +0545

    Added the superuser

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Genre)
+
+class BookAdmin(admin.ModelAdmin):
+	list_display=['title', 'author', 'display_genre']
+
+admin.site.register(Book, BookAdmin)
+
+admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ['last_name', 'first_name', 'date_of_birth', 'date_of_death']
+
+admin.site.register(Author, AuthorAdmin)
+
+
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 import uuid
 
+
 class Genre(models.Model):
 	name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Ficion, French Poetry, etc.)')
 
@@ -11,7 +12,7 @@ class Genre(models.Model):
 
 class Book(models.Model):
 	title = models.CharField(max_length=200)		
-	author = models.ForeignKey('Author', on_delete=models.SET_NULL, ,null=True)
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 	# Author as a string rather than object because it hasn't been declared yet in the file
 	summary = models.TextField(max_length=1000, help_text='Enter a brief descrption of the book')
 	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character ISBN Number')
@@ -23,6 +24,12 @@ class Book(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('book-detail', args=[str(self.id)])	
+
+	def display_genre(self):
+		# This creates a string for genre. This is required to display genre in Admin	
+		return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+	display_genre.short_description = 'Genre'		
 
 
 class BookInstance(models.Model):
@@ -36,9 +43,9 @@ class BookInstance(models.Model):
 		('o', 'On Loan'),
 		('a', 'Available'),
 		('r', 'Reserved'),
-		)
+	)
 
-	status = models.CharField(max_length=1, choices='LOAN_STATUS', default='m')
+	status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book Availability')
 
 	class Meta:
 		ordering = ['due_back']
@@ -46,6 +53,23 @@ class BookInstance(models.Model):
 	def __str__(self):
 		return '{0} ({1})'.format(self.id, self.book.title)	
 		
+
+class Author(models.Model):
+	first_name = models.CharField(max_length=100)				
+	last_name = models.CharField(max_length=100)				
+	date_of_birth = models.DateField(null=True, blank=True)
+	date_of_death = models.DateField('Died', null=True, blank=True)
+
+	class Meta:
+		ordering = ['last_name', 'first_name']
+
+	def get_absolute_url(self):
+		return reverse('author-detail', args=[str(self.id)])	
+
+	def __str__(self):
+		return '{0}, {1}'.format(self.last_name,self.first_name)	
+
+
 
 
 

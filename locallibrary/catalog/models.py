aff7a111cc51db84bcc087685780ee906c254f44
NimeshNayaju <nimeshnayaju@gmail.com>
2018-04-27 15:15:49 +0545

    Added some class

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,28 +2,52 @@ from django.db import models
 from django.urls import reverse
 import uuid
 
-# Create your models here.
 class Genre(models.Model):
-	name = models.CharField(max_length=100, help_text='Enter a Book Genre (e.g Science Fiction, French Poetry, etc)')
-
+	name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Ficion, French Poetry, etc.)')
 
 	def __str__(self):
 		return self.name
 
 
 class Book(models.Model):
-	title = models.CharField(max_length=200)
-	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-	summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-	isbn = models.CharField('ISBN', max_length=13. help_text='Enter the 13 digits ISBN number')
-	genre = models.ManyToManyField(Genre, help_text='Select the genre of the book')
+	title = models.CharField(max_length=200)		
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, ,null=True)
+	# Author as a string rather than object because it hasn't been declared yet in the file
+	summary = models.TextField(max_length=1000, help_text='Enter a brief descrption of the book')
+	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character ISBN Number')
+	genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+	# Genre has been defined so we can specify the object
 
 	def __str__(self):
 		return self.title
 
-
 	def get_absolute_url(self):
-		return reverse('book-detail', args=[str(self.id)])
+		return reverse('book-detail', args=[str(self.id)])	
 
 
-class BookInstance(models.Model)
+class BookInstance(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for the particular book across the entire library')
+	book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+	imprint = models.CharField(max_length=200)
+	due_back = models.DateField(null=True, blank=True)
+
+	LOAN_STATUS = (
+		('m', 'Maintenance'),
+		('o', 'On Loan'),
+		('a', 'Available'),
+		('r', 'Reserved'),
+		)
+
+	status = models.CharField(max_length=1, choices='LOAN_STATUS', default='m')
+
+	class Meta:
+		ordering = ['due_back']
+
+	def __str__(self):
+		return '{0} ({1})'.format(self.id, self.book.title)	
+		
+
+
+
+
+
